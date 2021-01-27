4290a55860899f1c87b6a7286fd4fe80309b7bef
talltotal <talltotal@163.com>
2021-01-27 13:37:12 +0800

    change git info

--- build/update-docs.sh
+++ build/update-docs.sh
@@ -7,12 +7,20 @@ yarn && yarn build
 # Publish to GitHub Pages
 cd .vuepress/dist
 git init
+
+# GitHub will only show you the token once, so be sure to copy it.
+token=$GITHUB_TOKEN
+if [ $token ];then
+    git config --global user.email "talltotal@163.com"
+    git config --global user.name "talltotal"
+fi
 git add -A
 git commit -m '[vuepress] update docs'
-if [[ -z "$ACCESS_TOKEN" ]];then
-    git push -f git@github.com:talltotal/how-it-works.git master:gh-pages
+if [ $token ];then
+    DEPLOY_REPO="https://user:${token}@github.com/${GITHUB_REPOSITORY}.git"
+    git push -f $DEPLOY_REPO master:gh-pages
 else
-    git push -f --force https://${ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git master:gh-pages
+    git push -f git@github.com:talltotal/how-it-works.git master:gh-pages
 fi
 
 # Cleanup
